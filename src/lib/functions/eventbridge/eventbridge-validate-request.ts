faa121294f9f855cab98d786d9c670c0b33a5035
Paul Visscher <p.e.visscher@skyleague.io>
2022-08-25 15:28:35 +0200

    feat(events): add eventbridge support

---
+++ src/lib/functions/eventbridge/eventbridge-validate-request.ts
@@ -0,0 +1,22 @@
+import type { EventBridgeEvent, EventBridgeHandler } from '../../events/eventbridge/types'
+
+import type { Either } from '@skyleague/axioms'
+import type { ErrorObject } from 'ajv'
+
+export function eventBridgeValidateRequest<E = unknown>() {
+    return {
+        before: (eventBridge: EventBridgeHandler, event: EventBridgeEvent<E>): Either<ErrorObject[], EventBridgeEvent<E>> => {
+            if (eventBridge.schema.detail?.is(event) === false) {
+                return { left: eventBridge.schema.detail?.validate.errors ?? [] }
+            }
+            return {
+                right: {
+                    detail: event.detail,
+                    source: event.source,
+                    detailType: event.detailType,
+                    raw: event.raw,
+                },
+            }
+        },
+    }
+}
