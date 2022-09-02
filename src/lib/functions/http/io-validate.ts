ae391c48282ec1e55eee2113bd0fba1d49060e10
Paul Visscher <p.e.visscher@skyleague.io>
2022-09-02 15:12:52 +0200

    feat(http): validate body, header, query, and pathParams

---
+++ src/lib/functions/http/io-validate.ts
@@ -0,0 +1,26 @@
+import type { HttpEventHandler, HttpRequest } from '../../events/http/types'
+
+import type { ErrorObject } from 'ajv'
+
+export function httpIOValidate() {
+    return {
+        before: (http: HttpEventHandler, event: HttpRequest): { left: ErrorObject[]; in: string } | { right: HttpRequest } => {
+            if (http.schema.body?.is(event.body) === false) {
+                return { left: http.schema.body.validate.errors ?? [], in: 'body' }
+            }
+            if (http.schema.query?.is(event.query) === false) {
+                return { left: http.schema.query.validate.errors ?? [], in: 'query' }
+            }
+            if (http.schema.pathParams?.is(event.pathParams) === false) {
+                return { left: http.schema.pathParams.validate.errors ?? [], in: 'path' }
+            }
+            if (http.schema.headers?.is(event.headers) === false) {
+                return { left: http.schema.headers.validate.errors ?? [], in: 'headers' }
+            }
+
+            return {
+                right: event,
+            }
+        },
+    }
+}
