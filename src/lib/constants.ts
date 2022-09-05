5aeeed9f46e2f64772bdb2e6e2e20d1a9d11f6e8
Mick van Duijn <m.vanduijn@zefiros.io>
2022-09-05 17:19:45 +0200

    fix(http-error): only expose stack when IS_DEBUG is explicitly true

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
 export const serviceName = process.env.SERVICE_NAME ?? 'unknown-service'
 export const namespace = process.env.SERVICE_NAMESPACE ?? 'unknown-service'
+export const isDebug = process.env.IS_DEBUG === 'true'
