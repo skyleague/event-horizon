2c698711475af62aa43ddbd09ca4a43774a1758b
Paul Visscher <p.e.visscher@skyleague.io>
2022-09-07 14:18:55 +0200

    fix: move secretsmanager import to selective client import

--- src/services/secret-manager/secret-manager.ts
+++ src/services/secret-manager/secret-manager.ts
@@ -1,6 +1,6 @@
 import { tracer } from '../../observability/tracer/tracer'
 
 import { memoize } from '@skyleague/axioms'
-import { SecretsManager } from 'aws-sdk'
+import SecretsManager from 'aws-sdk/clients/secretsmanager'
 
 export const createSecretManager = memoize<SecretsManager>(() => tracer.captureAWSClient(new SecretsManager()))
