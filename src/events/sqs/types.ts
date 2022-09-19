92cd4c52f0595e49c5a042d9d155aa7bbc7159d1
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-09-19 14:20:17 +0200

    fix(sqs): set correct handler event type (#49)

--- src/events/sqs/types.ts
+++ src/events/sqs/types.ts
@@ -12,7 +12,7 @@ export interface SQSEventHandler<C = never, S = never, P = unknown> {
     schema: {
         payload?: Schema<P>
     }
-    handler: (payload: P, context: LambdaContext<C, S>) => Promise<void> | void
+    handler: (payload: SQSEvent<P>, context: LambdaContext<C, S>) => Promise<void> | void
     payloadType?: 'binary' | 'json' | 'plaintext'
 }
 
