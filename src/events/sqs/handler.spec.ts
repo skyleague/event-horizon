0760f8260041d3656075383e0d7a57c30ea584dc
Mick van Duijn <m.vanduijn@zefiros.io>
2022-09-27 17:00:41 +0200

    fix(sqs): use the correct logging header

---
+++ src/events/sqs/handler.spec.ts
@@ -0,0 +1,32 @@
+import { handleSqsEvent } from './handler'
+
+import { random } from '@skyleague/axioms'
+import { arbitraryContext } from '@skyleague/space-junk'
+import type { SQSRecord } from 'aws-lambda'
+
+test('batchItemFailures', async () => {
+    await expect(
+        handleSqsEvent(
+            {
+                sqs: {
+                    schema: {},
+                    // eslint-disable-next-line @typescript-eslint/require-await
+                    handler: async () => {
+                        throw new Error('wut')
+                    },
+                },
+            },
+
+            [{ messageId: 'message-x' } as SQSRecord],
+            random(await arbitraryContext({ config: {}, services: {} }))
+        )
+    ).resolves.toMatchInlineSnapshot(`
+        {
+          "batchItemFailures": [
+            {
+              "itemIdentifier": "message-x",
+            },
+          ],
+        }
+    `)
+})
