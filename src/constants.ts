65242d13bb76382ce4015ef2086cd19d77b35c2f
Paul Visscher <p.e.visscher@skyleague.io>
2022-11-27 15:22:09 +0100

    refactor(config): uniformize environment variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,16 @@
-const isDebug = process.env.IS_DEBUG === 'true'
+const isDebug = (process.env.EH_IS_DEBUG ?? process.env.IS_DEBUG) === 'true'
+const logEventPayload = process.env.EH_LOG_EVENT_PAYLOAD ?? process.env.LOG_EVENT_PAYLOAD
+const logResultPayload = process.env.EH_LOG_RESULT_PAYLOAD ?? process.env.LOG_RESULT_PAYLOAD
 export const constants = {
-    serviceName: process.env.SERVICE_NAME ?? 'unknown-service',
-    namespace: process.env.SERVICE_NAMESPACE ?? 'unknown-service',
+    serviceName: process.env.EH_SERVICE_NAME ?? process.env.SERVICE_NAME ?? 'unknown-service',
+    namespace: process.env.EH_SERVICE_NAMESPACE ?? process.env.SERVICE_NAMESPACE ?? 'unknown-service',
     isDebug,
-    logEventPayload: process.env.LOG_EVENT_PAYLOAD === 'true' || (isDebug && process.env.LOG_EVENT_PAYLOAD !== 'false'),
-    logResultPayload: process.env.LOG_RESULT_PAYLOAD === 'true' || (isDebug && process.env.LOG_RESULT_PAYLOAD !== 'false'),
+    logEventPayload: logEventPayload === 'true' || (isDebug && logEventPayload !== 'false'),
+    logResultPayload: logResultPayload === 'true' || (isDebug && logResultPayload !== 'false'),
 
-    requestIdGenerator: process.env.REQUEST_ID_GENERATOR === 'uuid' ? 'uuid' : 'uuid',
-    requestIdHeader: process.env.REQUEST_ID_HEADER ?? 'x-amz-request-id',
-    traceIdGenerator: process.env.TRACE_ID_GENERATOR === 'uuid' ? 'uuid' : 'uuid',
-    traceIdHeader: process.env.TRACE_ID_HEADER ?? 'x-trace-id',
+    requestIdGenerator: (process.env.EH_REQUEST_ID_GENERATOR ?? process.env.REQUEST_ID_GENERATOR) === 'uuid' ? 'uuid' : 'uuid',
+    requestIdHeader: process.env.EH_REQUEST_ID_HEADER ?? process.env.REQUEST_ID_HEADER ?? 'x-amz-request-id',
+    traceIdGenerator: (process.env.EH_TRACE_ID_GENERATOR ?? process.env.TRACE_ID_GENERATOR) === 'uuid' ? 'uuid' : 'uuid',
+    traceIdHeader: process.env.EH_TRACE_ID_HEADER ?? process.env.TRACE_ID_HEADER ?? 'x-trace-id',
     eagerHandlerInitialization: (process.env.EH_EAGER_HANDLER_INIT ?? process.env.EAGER_HANDLER_INIT) === 'true',
 }
