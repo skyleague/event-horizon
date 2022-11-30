e3d99162545943c4d51e003dd7d389fe59a3891a
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-11-30 13:20:22 +0100

    feat(json): add prototype safe json parsing (#88)
    * refactor(config): uniformize environment variables
    
    * feat(validation): allow manual statuscode on validation error
    
    * feat(json): add prototype safe json parsing

--- src/events/http/functions/parse-event.ts
+++ src/events/http/functions/parse-event.ts
@@ -1,3 +1,4 @@
+import { parseJSON } from '../../../parsers'
 import type { HTTPEventHandler, HTTPRequest } from '../types'
 
 import { isString } from '@skyleague/axioms'
@@ -9,7 +10,7 @@ export function httpParseEvent({ bodyType = 'json' }: HTTPEventHandler) {
             let body: unknown = event.body
             if (bodyType !== 'binary' && isString(event.body)) {
                 const unencodedBody = event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString() : event.body
-                body = bodyType === 'json' ? JSON.parse(unencodedBody) : unencodedBody
+                body = bodyType === 'json' ? parseJSON(unencodedBody) : unencodedBody
             }
             const headers = Object.fromEntries(Object.entries(event.headers ?? {}).map(([h, v]) => [h.toLowerCase(), v]))
 
--- src/events/sqs/functions/parse-event.ts
+++ src/events/sqs/functions/parse-event.ts
@@ -1,3 +1,4 @@
+import { parseJSON } from '../../../parsers'
 import type { SQSEvent, SQSEventHandler } from '../types'
 
 import type { SQSRecord } from 'aws-lambda'
@@ -7,7 +8,7 @@ export function sqsParseEvent({ payloadType = 'json' }: SQSEventHandler) {
         before: (event: SQSRecord): SQSEvent => {
             let payload: unknown = event.body
             if (payloadType !== 'plaintext') {
-                payload = payloadType === 'json' ? JSON.parse(event.body) : payload
+                payload = payloadType === 'json' ? parseJSON(event.body) : payload
             }
             return {
                 payload: payload,
