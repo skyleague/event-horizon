142f4dc2614a793e733959eec2a0834f2a0d5a16
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-11-30 13:18:22 +0100

    refactor(config): uniformize environment variables (#86)

--- examples/pet-store/functions/create-pet/handler.spec.ts
+++ examples/pet-store/functions/create-pet/handler.spec.ts
@@ -6,8 +6,8 @@ import { forAll, tuple } from '@skyleague/axioms'
 import { context, httpEvent } from '@skyleague/event-horizon-dev'
 
 test('handler', async () => {
-    forAll(tuple(httpEvent(handler as any), await context()), ([x, ctx]) => {
-        const { statusCode, body } = handler.http.handler(x as any, ctx)
+    forAll(tuple(httpEvent(handler), await context()), ([x, ctx]) => {
+        const { statusCode, body } = handler.http.handler(x, ctx)
         return statusCode === 200 && Pet.is(body)
     })
 })
