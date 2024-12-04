633d533f6b3c03d5568cfb0bdb4a5f71146ef1dc
Paul Visscher <p.e.visscher@skyleague.io>
2024-12-04 16:21:55 +0100

    feat: update to node 22

--- src/events/s3-batch/types.ts
+++ src/events/s3-batch/types.ts
@@ -1,4 +1,5 @@
-import type { Promisable, Try } from '@skyleague/axioms'
+import type { Try } from '@skyleague/axioms'
+import type { Promisable } from '@skyleague/axioms/types'
 import type { S3BatchEvent, S3BatchEventTask, S3BatchResultResultCode } from 'aws-lambda'
 import type { InferFromParser, MaybeGenericParser } from '../../parsers/types.js'
 import type { EventHandlerDefinition, LambdaContext } from '../types.js'
