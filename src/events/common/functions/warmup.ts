c64266c548e233e0edd1d4dc594c50092f37ef62
pevisscher <2388946+pevisscher@users.noreply.github.com>
2025-05-09 16:41:32 +0200

    feat: allow warmer object events (#242)

--- src/events/common/functions/warmup.ts
+++ src/events/common/functions/warmup.ts
@@ -1,7 +1,8 @@
+import { isObject } from '@skyleague/axioms'
 import type { RawRequest } from '../raw-aws.js'
 
 export function warmup({
-    isWarmingUp = (request) => request === '__WARMER__',
+    isWarmingUp = (request) => request === '__WARMER__' || (isObject(request) && '__WARMER__' in request),
 }: {
     isWarmingUp?: (request: RawRequest | string) => boolean
 } = {}) {
