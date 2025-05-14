ff8c874a8cca2c58e533491f64116d673f44e664
pevisscher <2388946+pevisscher@users.noreply.github.com>
2025-05-14 14:09:45 +0200

    refactor: replace Therefore Schema import with interface (#243)

--- src/parsers/types.ts
+++ src/parsers/types.ts
@@ -1,12 +1,17 @@
-import type { Schema } from '@skyleague/therefore'
+import type { ErrorObject } from 'ajv'
 import type { StandardSchemaV1 } from '../errors/event-error/standard-schema.js'
 
-export type GenericParser = StandardSchemaV1 | Schema<unknown>
+interface ThereforeSchema<T> {
+    is: (o: unknown) => o is T
+    errors?: ErrorObject[] | null | undefined
+}
+
+export type GenericParser = StandardSchemaV1 | ThereforeSchema<unknown>
 export type MaybeGenericParser = GenericParser | undefined
 export type InferFromParser<T extends MaybeGenericParser, Default = unknown> = [T] extends [undefined]
     ? Default
-    : T extends Schema<infer U>
-      ? U
-      : T extends StandardSchemaV1
-        ? StandardSchemaV1.InferOutput<T>
+    : T extends StandardSchemaV1
+      ? StandardSchemaV1.InferOutput<T>
+      : T extends ThereforeSchema<infer U>
+        ? U
         : Default
