e15bbd6d83b53cabed750ca251d565c79b11462e
pevisscher <2388946+pevisscher@users.noreply.github.com>
2025-05-21 14:38:12 +0200

    feat: add WARMER_EVENT constant and export it (#245)

--- src/events/common/functions/warmup.ts
+++ src/events/common/functions/warmup.ts
@@ -2,6 +2,7 @@ import { isObject } from '@skyleague/axioms'
 import type { RawRequest } from '../raw-aws.js'
 
 export const WARMER_EVENT_KEY = '__WARMER__'
+export const WARMER_EVENT = { [WARMER_EVENT_KEY]: 'true' }
 
 export function warmup({
     isWarmingUp = (request) => request === WARMER_EVENT_KEY || (isObject(request) && WARMER_EVENT_KEY in request),
--- src/events/common/index.ts
+++ src/events/common/index.ts
@@ -1,3 +1,3 @@
 export type { ProfileSchema } from './functions/profile-handler.js'
 export type { EventHandler } from './types.js'
-export { WARMER_EVENT_KEY } from './functions/warmup.js'
+export { WARMER_EVENT, WARMER_EVENT_KEY } from './functions/warmup.js'
